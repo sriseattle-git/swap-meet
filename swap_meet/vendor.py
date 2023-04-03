b32cd35e12e49b527d068d728def45f0d3ce56e8
Sriram R <sriram@msn.com>
2023-04-03 14:27:28 -0700

    Wave 1 unit tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = []
+        if inventory:
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+    
+
+
